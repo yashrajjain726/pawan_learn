71107dde3f0564c857b7b02f809ee272c36158b3
Yashraj Jain <yashrajjain726@gmail.com>
2020-12-03 22:31:37 +0530

    Card, Asset Images, TextField & ScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,25 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "Change My Name";
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +37,35 @@ class HomePage extends StatelessWidget {
         title: Text("Awesome App"),
       ),
       body: Center(
-        child: Container(
-          color: Colors.teal,
-          height: 100,
-          width: 100,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: [
+                  Image.asset("assets/coding.jpg"),
+                  SizedBox(height: 20.0),
+                  Text(
+                    myText,
+                    style:
+                        TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20.0),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        hintText: "Enter Text",
+                        labelText: "Name",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
       drawer: Drawer(
@@ -65,8 +108,11 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.edit),
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
